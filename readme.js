86ea4d1c17108aa52bbd75e444f290eca334056a
mahlon1200 <mahlonmahlonte@gmail.com>
2025-05-09 11:32:40 +0300

    Create readme.js

---
+++ readme.js
@@ -0,0 +1,87 @@
+
+<a><img src='(https://imgur.com/a/clyGjCl)'/></a>
+
+
+<p align="center">                                             𝑴𝑨𝑯𝑳𝑶𝑵-𝑴𝑫
+
+
+
+</p>
+<p align="center"> 
+  <a href="[https://whatsapp.com/channel/0029VajQn6YF1YlPE0XgBC2m](https://whatsapp.com/channel/0029VbAekzRFsn0WcsKdGa1f)">
+    
+
+
+ 
+[![Typing SVG](https://readme-typing-svg.herokuapp.com?font=Rockstar-ExtraBold&size=30&pause=1000&color=0000FF&center=true&vCenter=true&width=500&height=60&lines=WELCOME+TO+𝑴𝑨𝑯𝑳𝑶𝑵-𝑴𝑫!)](https://git.io/typing-svg)
+
+   [![Typing SVG](https://readme-typing-svg.herokuapp.com?font=Rockstar-ExtraBold&color=F33A6A&lines=FORK+AND+MAYBE+GIVE+ME+A+STAR)](https://git.io/typing-svg)
+
+
+ 
+
+<p align="center">
+
+[!𝑴𝑨𝑯𝑳𝑶𝑵-𝑴𝑫 TECH KENYA](https://github.com/mahlon.png?lenght=50width=50)](https://github.com/mahlon)
+</p>
+
+<p align="center">
+<a href="#"><img title="Creator" src="https://img.shields.io/badge/Creator-𝑴𝑨𝑯𝑳𝑶𝑵-blue.svg?style=for-the-badge&logo=github"></a>
+</p>
+<p align="center">
+<a href="https://github.com/Mahlon1200?tab=followers"><img title="Followers" src="https://img.shields.io/github/followers/mahlon1200?label=Followers&style=social"></a>
+<a href="https://github.com/mahlon1200/𝑴𝑨𝑯𝑳𝑶𝑵-𝑴𝑫/stargazers/"><img title="Stars" src="https://img.shields.io/github/stars/mahlon1200/𝑴𝑨𝑯𝑳𝑶𝑵-𝑴𝑫?&style=social"></a>
+<a href="https://github.com/mahlon1200/𝑴𝑨𝑯𝑳𝑶𝑵-𝑴𝑫/network/members"><img title="Fork" src="https://img.shields.io/github/forks/mahlon1200/𝑴𝑨𝑯𝑳𝑶𝑵-𝑴𝑫?style=social"></a>
+<a href="https://github.com/mahlon1200/𝑴𝑨𝑯𝑳𝑶𝑵-𝑴𝑫/watchers"><img title="Watching" src="https://img.shields.io/github/watchers/mahlon1200/𝑴𝑨𝑯𝑳𝑶𝑵-𝑴𝑫?label=Watching&style=social"></a>
+</p>
+ 
+
+## ```CONTACT ME```
+
+<p align="center">
+
+<a href="https://api.whatsapp.com/send?phone=254737425382&text=Hello+𝑴𝑨𝑯𝑳𝑶𝑵-𝑴𝑫"><img src="https://img.shields.io/badge/Contact 𝑴𝑨𝑯𝑳𝑶𝑵-𝑴𝑫༆-25D366?style=for-the-badge&logo=whatsapp&logoColor=white" />
+
+
+***This bot is created with the help of NodeJS and uses [Baileys](https://github.com/whiskeysockets/Baileys)***
+
+## FEATURES
+This is a highly customisable simple whatsapp bot with group management features and few media commands and chatgpt courtesy of Openai.
+
+Our AI features use random APIs to run, so sometimes they may be down.
+
+# SET-UP
+
+## ` Fork this repo`
+<p align="centre">
+<a href="https://github.com/mahlon1200/MAHLON-MD/fork"><img src="https://img.shields.io/badge/Fork%20Create-purple?style=for-the-badge&logo=github" alt="FORK 𝑴𝑨𝑯𝑳𝑶𝑵-𝑴𝑫" width="160"></a>
+<p/>
+  
+Linking/Pairing:
+## ` Pair onrender`
+[![SESSION FOR 𝑴𝑨𝑯𝑳𝑶𝑵-𝑴𝑫](https://img.shields.io/badge/_𝗦𝗘𝗦𝗦𝗜𝗢𝗡_(𝑴𝑨𝑯𝑳𝑶𝑵-𝑴𝑫)-E6E6FA?style=for-the-badge&logo=koyeb)](https://mahlon-md-session-2.onrender.com)  
+
+###  𝗗𝗘𝗣𝗟𝗢𝗬 𝗢𝗡 𝗛𝗘𝗥𝗢𝗞𝗨:
+
+
+ ***[![Tap to deploy on heroku](https://www.herokucdn.com/deploy/button.svg)](https://dashboard.heroku.com/new?button-url=https://github.com/mahlon1200/MZAZI-XMD&template=https://github.com/mahlon1200/𝑴𝑨𝑯𝑳𝑶𝑵-𝑴𝑫.git)***
+ 
+
+    
+
+- Fill in the required variables into your hosting site for bot to work.
+ </h2>
+     
+
+    
+ 
+
+
+
+
+
+## License
+
+[MIT License](https://github.com/mahlon1200/MAHLON-MD/blob/main/LICENSE)
+
+Copyright (c) 2025 𝑴𝑨𝑯𝑳𝑶𝑵-𝑴𝑫 
